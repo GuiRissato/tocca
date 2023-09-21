1adbd4fdf9856410600e5b72b963b4af4d2ab50c
GuiRissato <guilherme_andrade_rissato@hotmail.com>
2023-09-21 19:26:21 -0300

    login, user list and store

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,8 @@
 // src/components/MainPage.tsx
 import React, { useEffect, useState } from 'react';
-import UserList from '../../components/UserListProps';
+import UserList from '../../components/UserListProps'; 
+import UserUpdateModal from '../../components/UserUpdateModal'; 
 import api from '../../interface/API';
-
-interface UserListProps {
-  users: UserData[]; 
-}
 
 interface UserData {
   id: number;
@@ -16,7 +13,9 @@ interface UserData {
 
 const Main: React.FC = () => {
   const [users, setUsers] = useState<UserData[]>([]);
-  
+  const [selectedUser, setSelectedUser] = useState<any>(null);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+
   async function fetchUsers() {
     try {
       const response = await api.get('/users');
@@ -30,24 +29,33 @@ const Main: React.FC = () => {
       console.error('Erro ao obter a lista de usuários', error);
     }
   }
+
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [selectedUser,users]);
+
+  const handleEditUserClick = (user: any) => {
+    setSelectedUser({...user, password: ''});
+    setIsUpdateModalOpen(true);
+  };
 
   return (
-  <>
-    { users.length == 0 
-      ?
-      <div>Carregando...</div>
-      :
-        <div>
-          <UserList users={users} /> {/* Renderize o componente UserList com os dados dos usuários */}
-        </div>
+    <>
+      <h1>Lista de Usuários</h1>
+      {users.length === 0 ? (
+        <div>Carregando...</div>
+      ) : (
+        <UserList users={users} onEditUserClick={handleEditUserClick} />
+      )}
 
-    }
-
-
-  </>
+      {selectedUser && (
+        <UserUpdateModal
+          isOpen={isUpdateModalOpen}
+          onClose={() => setIsUpdateModalOpen(false)}
+          user={selectedUser}
+        />
+      )}
+    </>
   );
 };
 
